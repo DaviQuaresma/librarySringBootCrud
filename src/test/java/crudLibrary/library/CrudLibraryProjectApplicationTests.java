eaad6714f67ad1f42159bfb552bb6b510e3bbfb8
Davi Quaresma da Silva Henriques <102613671+DaviQuaresma@users.noreply.github.com>
2025-08-28 14:27:17 -0300

    Init: Estrutura padrão do Spring Boot

---
+++ src/test/java/crudLibrary/library/CrudLibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package crudLibrary.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
